e94e629a4014a30ff75d67f3fd3d53af33facaf1
Jaimin Patel <caprizone6@gmail.com>
2023-08-01 06:34:20 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,29 @@
+let set_hand = 0
+input.onGesture(Gesture.Shake, function () {
+    set_hand = randint(1, 3)
+    if (set_hand == 1) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else if (set_hand == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+})
